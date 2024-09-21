738504f52d04fb8ef2c50e0a3d90056a81f49c04
Mohit kumar <itsmohit3005@gmail.com>
2024-09-21 19:26:39 +0530

    getBean by type

--- src/main/java/online/devplanet/App.java
+++ src/main/java/online/devplanet/App.java
@@ -8,9 +8,18 @@ public class App
     public static void main(String[] args)
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj1 = (Alien) context.getBean("alien1");
+        Alien obj1 =  context.getBean("alien1", Alien.class);
 
         obj1.code();
-        Desktop obj2 = (Desktop) context.getBean("comp2");
+
+        // Retrieve a bean of type Computer from the Spring container
+        // (by its class type)
+
+//       Potential Issue: Multiple Beans of the Same Type:
+//		If Computer is an interface and both Laptop and Desktop implement Computer, calling context.getBean(Computer.class) will lead to ambiguity.
+//		You can resolve this by - Specify a primary bean: Mark one of the beans as primary using the @Primary annotation or the primary attribute in XML.
+
+        Computer comp = context.getBean(Computer.class);
+        Desktop obj2 = context.getBean(Desktop.class);
     }
 }
