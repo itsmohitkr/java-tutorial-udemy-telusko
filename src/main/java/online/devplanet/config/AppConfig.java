fef75937b739bc5f886ddbc65f2ec262b8a3deee
Mohit kumar <itsmohit3005@gmail.com>
2024-09-22 09:03:26 +0530

    primary and qualifier in java based config

--- src/main/java/online/devplanet/config/AppConfig.java
+++ src/main/java/online/devplanet/config/AppConfig.java
@@ -3,13 +3,19 @@ package online.devplanet.config;
 import online.devplanet.Alien;
 import online.devplanet.Computer;
 import online.devplanet.Desktop;
+import online.devplanet.Laptop;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AppConfig {
 
     @Bean
+    //  now we have 2 beans:
+    // 1st way: use @Qualifier("laptop") here :     public Alien alien(@Qualifier("laptop") Computer comp){
+    // 2nd way: use @Primary
     public Alien alien(Computer comp){
         Alien obj =new Alien();
         obj.setAge(24);
@@ -21,6 +27,12 @@ public class AppConfig {
     @Bean
     public Desktop desktop(){
         return new Desktop();
+    }
+
+    @Bean
+    @Primary
+    public Laptop laptop(){
+        return new Laptop();
     }
 
 }
